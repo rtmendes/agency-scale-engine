27c477fa6bc2621d637b1e65114a00e050b6e801
rtmendes <85340359+rtmendes@users.noreply.github.com>
2026-04-27 22:21:40 -0400

    Add src/pages/index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { LandingPage } from "./LandingPage";
+export { DashboardPage } from "./DashboardPage";
+export { ModulePage } from "./ModulePage";
+export { PromptsPage } from "./PromptsPage";
+export { ResourcesPage } from "./ResourcesPage";
+export { LoginPage } from "./LoginPage";
+export { SignupPage } from "./SignupPage";
+export { SettingsPage } from "./SettingsPage";
